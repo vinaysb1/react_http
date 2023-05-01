ab0b0a99c3bede8eda493d5af59b75c8dc551edd
vinay <vinaykumarbeelagi@gmail.com>
2023-05-01 20:41:16 +0530

    react hooks use state &prevstate

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import PostList from './Components/PostList';
 import PostForm from './Components/PostForm';
 import HookCounter from './Components/HookCounter';
+import HookCounterTwo from './Components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter/>
+      <HookCounterTwo/>
+      {/* <HookCounter/> */}
       {/* <PostForm /> */}
     {/* <PostList /> */}
     </div>
