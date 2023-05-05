e631e8313047f2cf3b9c8b21e4d361e1989365c3
vinay <vinaykumarbeelagi@gmail.com>
2023-05-05 23:58:59 +0530

    HookMouse

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import PostForm from './Components/PostForm';
 import HookCounter from './Components/HookCounter';
 import HookCounterTwo from './Components/HookCounterTwo';
 import HookCounterOne from './Components/HookCounterOne';
+import HookMouse from './Components/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne/>
+      <HookMouse />
+      {/* <HookCounterOne/> */}
       {/* <HookCounterTwo/> */}
       {/* <HookCounter/> */}
       {/* <PostForm /> */}
